2db85e1f9893a6f426c6c630b029308ec1ccb00a
chf <fangc86@gmail.com>
2014-06-16 21:49:10 -0400

    send commit

--- OurWork/ModifiedInput_061514.m
+++ OurWork/ModifiedInput_061514.m
@@ -1,7 +1,7 @@
 function [mean,input2] = ModifiedInput_061514(j)
 input2 = [];
 mean = [];
-for i = 1:12
+for i = 1:120
     [X,Y] = MeanWithNan_061514(j(:,i));
     mean = [mean X];
     input2 = [input2 Y];
