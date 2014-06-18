6c205f6ba306e549a5654a0e2a8f34982b3063b1
chf <fangc86@gmail.com>
2014-06-17 23:08:59 -0400

    commit_061714

--- OurWork/MeanWithNan_061514.m
+++ OurWork/MeanWithNan_061514.m
@@ -1,8 +1,8 @@
-function [m,i] =MeanWithNan_061514(i)
-boolean = isnan(i);
-NumNotNan = length(i)- sum(boolean);
-i(boolean) = 0;
-m = sum(i)/ NumNotNan;
-i(boolean)= m
+function [m,i] =MeanWithNan_061514(i)   
+boolean = isnan(i);                        % find the NaN values
+NumNotNan = length(i)- sum(boolean);       % number of non-NaN values per row/column
+i(boolean) = 0;                            % replace NaN with 0
+m = sum(i)/ NumNotNan;                     % calculate & return the mean value per row/column
+i(boolean)= m                              % replace initial NaN to mean value $ return the modified vector
 
 
--- OurWork/ModifiedInput_061514.m
+++ OurWork/ModifiedInput_061514.m
@@ -1,8 +1,8 @@
 function [mean,input2] = ModifiedInput_061514(j)
-input2 = [];
-mean = [];
-for i = 1:12
-    [X,Y] = MeanWithNan_061514(j(:,i));
-    mean = [mean X];
-    input2 = [input2 Y];
+input2 = [];                      % initiaize modified matrix
+mean = [];                        % initialize the mean vector
+for i = 1:length(j(1,:));                      % ovall 12 columns
+    [X,Y] = MeanWithNan_061514(j(:,i)); % calucate & return mean value and modified vector for each column
+    mean = [mean X];                    % generate a row vector of mean values
+    input2 = [input2 Y];                % generate modified matrix
 end
